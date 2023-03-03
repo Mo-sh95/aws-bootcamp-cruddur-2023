8eea14e2a1f9e785e859b1137856049fe8bd4ac7
Mo-sh95 <98752732+Mo-sh95@users.noreply.github.com>
2023-03-03 22:43:17 +0200

    added rollbar endpoint test

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -26,6 +26,12 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor
 #from aws_xray_sdk.core import xray_recorder
 #from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
+# Rollbar
+import os
+import rollbar
+import rollbar.contrib.flask
+from flask import got_request_exception
+
 # CloudWatch Logs
 #import watchtower
 #import logging
@@ -46,6 +52,24 @@ processor = BatchSpanProcessor(OTLPSpanExporter())
 provider.add_span_processor(processor)
 trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
+
+# Rollbar
+rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
+@app.before_first_request
+def init_rollbar():
+    """init rollbar module"""
+    rollbar.init(
+        # access token
+        rollbar_access_token,
+        # environment name
+        'production',
+        # server root directory, makes tracebacks prettier
+        root=os.path.dirname(os.path.realpath(__file__)),
+        # flask already sets up logging
+        allow_logging_basic_config=False)
+
+    # send exceptions from `app` to rollbar, using flask's signal system.
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
 
 app = Flask(__name__)
 # HoneyComb
@@ -169,6 +193,13 @@ def data_activities_reply(activity_uuid):
     return model['data'], 200
   return
 
+# Roll-bar test
+@app.route('/rollbar/test')
+def rollbar_test():
+    rollbar.report_message('Hello World!', 'warning')
+    return "Hello World!"
+  
+  
 if __name__ == "__main__":
   app.run(debug=True)
 
